5b41b588451d4dd5e2dde98be8b1fbeb164f8459
Rakeshbaghel021 <rkrkumar021@gmail.com>
2019-08-15 15:00:22 +0530

    update complete obj 1

--- 1. objects/2.js
+++ 1. objects/2.js
@@ -15,29 +15,30 @@ value[12]   ="mango";
 
   console.log(user["username"])
 // 4. Add a key of the value of variable `batch` in the object with the value of 10.
-var myBatch={
-	         batch:10
-            }
-
+// var myBatch={
+// 	         batch:10
+//             }
+user.myBatch=10
 
 // 5. Using the alert function alert the value of the key added above.
  
- alert(myBatch.batch)
+ alert(user.batch)
 
 // 6. Add a key of `42` to the object with a value of `The answer to the meaning of life 🧸`.
 
-myBatch[42]=`The answer to the meaning of life 🧸`;
+user[42]=`The answer to the meaning of life 🧸`;
 
 // 7. Using the function console.log log the value of the key `42`.
 
-console.log([42]);
+console.log(user[42]);
 
 // 8. Add a key in your object with the value of the variable `city`. The value of the key should be `true`.
 
-myBatch.city=`true`;
+user.city=`true`;
 
 // 9. Uisng console.log log the value of the key defined above.
 
-console.log(myBatch.city);
+console.log(user.city);
 
 // 10. Can you define a key of `let or var` in any object? Reason.
+The difference between them is that var is function scoped and let is block scoped. 
